6aa380c2bbdcc6935e4dd2dd2b0c3752af4007ab
Christianah Bepeh <132619322+Ianah-05@users.noreply.github.com>
2023-08-01 16:06:16 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
-		if (node || !(node->next))
+		if (!node || !(node->next))
 			return (-1);
 		node = node->next;
 		i++;
